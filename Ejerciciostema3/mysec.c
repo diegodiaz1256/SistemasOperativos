c15e57e9ce1526722428e17df6b51a002bc09961
diegodiaz1256 <fernandezdiego3758@gmail.com>
2021-10-15 15:11:55 +0200

    misec.c terminado

--- Ejerciciostema3/mysec.c
+++ Ejerciciostema3/mysec.c
@@ -1,4 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void num(char** argv);
+void firstlast(char** argv);
+void firststeplast(char** argv);
+
 int main(int argc,char ** argv){
+    switch (argc)
+    {
+    case 2:
+        /* code */
+        num(argv);
+        break;
+    case 3:
+        firstlast(argv);
+        break;
+    case 4:
+        firststeplast(argv);
+        break;
+    default:
+        printf("error de argumentos");
+        return 1;
+    }     
+    return 0;
+}
+
+void num(char** argv){
+    for(int i=1;i<=atoi(argv[1]);i++){
+        printf("%d\n",i);
+    }
+}
+void firstlast(char** argv){
+    for(int i=atoi(argv[1]);i<=atoi(argv[2]);i++){
+        printf("%d\n",i);
+    }
     
+}
+void firststeplast(char** argv){
+    int o= atoi(argv[2]);
+    for(int i=atoi(argv[1]);i<=atoi(argv[3]);i++){
+        if(o==atoi(argv[2])){
+            printf("%d\n",i);
+            o=0;
+        }
+        o++;
+    }
 }
